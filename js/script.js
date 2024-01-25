e819dabc4e7dc9a44fef04c08863730716e16b75
mario.arista <mario.arista.dev@gmail.com>
2024-01-25 14:55:07 +0100

    first attempt. Somenthing wrong in final syntax

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 document.getElementById('title').innerHTML = "Ciao Mario";
+
+// let introU;
+// introU = 
+prompt("Prima di accedere al sito devi creare un Password sicura")
+
+let surName;
+surName = prompt("Qual è il tuo Nome")
+
+let lastName;
+lastName = prompt("Qual è il tuo Cognome")
+
+let yourColor;
+yourColor = prompt("Qual è il tuo colore preferito")
+
+let finalPwd;
+finalPwd = document.getElementById("title").innerHTML = "Ecco la tua password sicura: + surName + lastName + yourColor";
